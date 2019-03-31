6b167e4fe588c605b8a2046d52e5487fd469454f
Starling Germosen <sgrysoft@outlook.com>
2019-03-31 12:04:41 -0300

    views component implementation on groups

---
+++ Tetas.Web/ViewComponents/GroupMemberViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tetas.Infraestructure;
+
+namespace Tetas.Web.ViewComponents
+{
+    public class GroupMemberViewComponent:ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GroupMemberViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var gMembers = await _context.GroupMembers.ToListAsync();
+
+            return View(gMembers);
+        }
+    }
+}
---
+++ Tetas.Web/ViewComponents/GroupPostViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tetas.Infraestructure;
+
+namespace Tetas.Web.ViewComponents
+{
+    public class GroupPostViewComponent:ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GroupPostViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var gPost = await _context.GroupPosts.ToListAsync();
+
+            return View(gPost);
+        }
+    }
+}
